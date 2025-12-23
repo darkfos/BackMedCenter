2f9a207878ed63e1adb7682f190cf2492338605f
darkfos82 <darkfos82@gmail.com>
2025-12-24 00:41:23 +0300

    feature: Добавил хеширование и верификацию пароля

--- src/module/users/service/user.service.ts
+++ src/module/users/service/user.service.ts
@@ -3,6 +3,7 @@ import { Repository } from "typeorm";
 import { dbSource } from '../../../db/data-source.js';
 import { User } from "../entity/User.entity.js";
 import { User as UserDTO } from "../dto/User.dto.js"
+import { hashPassword } from "../../../utils/other/hash_password.js";
 
 
 export class UserService {
@@ -10,8 +11,13 @@ export class UserService {
     protected static repository: Repository<User> = dbSource.getRepository(User);
 
     static async createUser(userData: UserDTO): Promise<User> {
+
+        const newHashedPassword = await hashPassword(userData.password);
+        userData.password = newHashedPassword;
+
         const repository: Repository<User> = dbSource.getRepository(User);
         const newUser = await repository.create(userData);
+
         return await repository.save(newUser);
     }
 
--- src/utils/other/hash_password.ts
+++ src/utils/other/hash_password.ts
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt';
 
-export function hashPassword(password: string) {
+export async function hashPassword(password: string) {
     return await bcrypt.hash(password, 14);
 }
 
-export function verifyPassword(password: string, hashedPassword: string) {
+export async function verifyPassword(password: string, hashedPassword: string) {
     return await bcrypt.compare(password, hashedPassword);
 }
